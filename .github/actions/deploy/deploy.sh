d43e40560aa6d349c949edcaa9744892cf266b3c
Nestor hdez <nestorhdez.26@gmail.com>
2020-07-12 15:56:27 +0100

    Remove BASE var and unused commands

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+set -e
+
 # Deploy a Vue.js project to Github pages
 
 # Your local folder of the project has to have the same name
@@ -5,8 +8,8 @@
 
 git config --local user.email "${INPUT_EMAIL}"
 git config --local user.name "${GITHUB_ACTOR}"
+echo git config setted.
 
-BASE=$(pwd)
 PROJECT=${PWD##*/}
 COMMIT=$(git log -1 --pretty=%B)
 
@@ -15,19 +18,15 @@ COMMIT=$(git log -1 --pretty=%B)
 git checkout gh-pages
 
 # Create vue.config file with the name of the project
-echo "module.exports = { publicPath: '$PROJECT' }" > $BASE/vue.config.js
+echo "module.exports = { publicPath: '$PROJECT' }" > ./vue.config.js
+echo Vue config created.
 
 # Comment the dist folder to push it to github
-sed -i -e 's/^\/dist.*/#\/dist/g' $BASE/.gitignore
+sed -i -e 's/^\/dist.*/#\/dist/g' ./.gitignore
 
 npm run build
 
 git add dist && git commit -m "$COMMIT"
 git subtree push --prefix dist origin gh-pages
-
-# Remove the comment to the dist folder
-sed -i -e 's/^#\/dist/\/dist/g' /Users/nestorhdez/Desktop/.gitignore
-
-rm $BASE/vue.config.js
 
 echo Deployed 🚀!
