2e86bfddaa76256ad13f5e0b79dbc40a85f94430
Nestor hdez <nestorhdez.26@gmail.com>
2020-07-12 16:07:10 +0100

    Change .gitignore path on action

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -6,28 +6,27 @@ set -e
 # Your local folder of the project has to have the same name
 # as the Github repository
 
-echo $(ls -a)
-# git config --local user.email "${INPUT_EMAIL}"
-# git config --local user.name "${GITHUB_ACTOR}"
-# echo git config setted.
+git config --local user.email "${INPUT_EMAIL}"
+git config --local user.name "${GITHUB_ACTOR}"
+echo git config setted.
 
-# PROJECT=${PWD##*/}
-# COMMIT=$(git log -1 --pretty=%B)
+PROJECT=${PWD##*/}
+COMMIT=$(git log -1 --pretty=%B)
 
-# [[ -n $COMMIT ]] || COMMIT="Initial dist subtree commit"
+[[ -n $COMMIT ]] || COMMIT="Initial dist subtree commit"
 
-# git checkout gh-pages
+git checkout gh-pages
 
-# # Create vue.config file with the name of the project
-# echo "module.exports = { publicPath: '$PROJECT' }" > ./vue.config.js
-# echo Vue config created.
+# Create vue.config file with the name of the project
+echo "module.exports = { publicPath: '$PROJECT' }" > ./vue.config.js
+echo Vue config created.
 
-# # Comment the dist folder to push it to github
-# sed -i -e 's/^\/dist.*/#\/dist/g' ./.gitignore
+# Comment the dist folder to push it to github
+sed -i -e 's/^\/dist.*/#\/dist/g' .gitignore
 
-# npm run build
+npm run build
 
-# git add dist && git commit -m "$COMMIT"
-# git subtree push --prefix dist origin gh-pages
+git add dist && git commit -m "$COMMIT"
+git subtree push --prefix dist origin gh-pages
 
-# echo Deployed 🚀!
+echo Deployed 🚀!
