20f6e5cc80ca8ce5229b73702a13349cb732f470
Nestor hdez <nestorhdez.26@gmail.com>
2020-07-12 16:47:02 +0100

    Remove checkout to gh-pages branch

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -11,16 +11,20 @@ git config --local user.name "${GITHUB_ACTOR}"
 echo git config setted.
 
 PROJECT=${PWD##*/}
+COMMIT=$(git log -1 --pretty=%B)
 
-[[ -n $INPUT_COMMIT ]] || INPUT_COMMIT="Initial dist subtree commit"
+[[ -n $COMMIT ]] || COMMIT="Initial dist subtree commit"
 
 # Create vue.config file with the name of the project
 echo "module.exports = { publicPath: '$PROJECT' }" > ./vue.config.js
 echo Vue config created.
 
+# Comment the dist folder to push it to github
+sed -i -e 's/^\/dist.*/#\/dist/g' .gitignore
+
 npm run build
 
-git add dist && git commit -m "$INPUT_COMMIT"
+git add dist && git commit -m "$COMMIT"
 git subtree push --prefix dist origin gh-pages
 
 echo Deployed 🚀!
