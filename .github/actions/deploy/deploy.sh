f697d68d5f52597f4b28c3ec7a1a43373c063b6e
Nestor hdez <nestorhdez.26@gmail.com>
2020-07-12 16:40:57 +0100

    Split deploy into 3 steps

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -11,11 +11,8 @@ git config --local user.name "${GITHUB_ACTOR}"
 echo git config setted.
 
 PROJECT=${PWD##*/}
-COMMIT=$(git log -1 --pretty=%B)
 
-[[ -n $COMMIT ]] || COMMIT="Initial dist subtree commit"
-
-git checkout gh-pages
+[[ -n $INPUT_COMMIT ]] || INPUT_COMMIT="Initial dist subtree commit"
 
 # Create vue.config file with the name of the project
 echo "module.exports = { publicPath: '$PROJECT' }" > ./vue.config.js
@@ -23,7 +20,7 @@ echo Vue config created.
 
 npm run build
 
-git add dist && git commit -m "$COMMIT"
+git add dist && git commit -m "$INPUT_COMMIT"
 git subtree push --prefix dist origin gh-pages
 
 echo Deployed 🚀!
